ff65697a01c1434f18620382726c0c8ed2767ec1
Andaar <igor.kozub@outlook.com>
2017-11-29 09:48:52 +0100

    Create asd.cpp

---
+++ asd.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+
+void drukuj(char t[8][8])
+{
+	for(int i = 0; i < 8; i++)
+	{
+		cout << "|";
+		for(int j = 0; j < 8; j++)
+		{
+			if(i%2==0 && t[i][j] == ' ' && j%2 == 0)
+			{
+				cout << (char)24 << "|";
+			}
+			else if(i%2!=0 && t[i][j] == ' ' && j%2 != 0)
+			{
+				cout << (char)24 << "|";
+			}
+			else
+			{
+				cout << t[i][j] << "|";
+			}
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	int const iloscWierszy = 8;
+	char t[iloscWierszy][iloscWierszy];
+	for(int i = 0; i < iloscWierszy; i++)
+	{
+		for(int j = 0; j < iloscWierszy; j++)
+		{
+			t[i][j] = ' ';
+		}
+	}
+	
+	drukuj(t);
+	return 0;
+}
